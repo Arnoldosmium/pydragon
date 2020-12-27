6730200aca75cef90b38e79ac38e27f5a11224e7
Arnoldosmium <eaglemewpsy@gmail.com>
2020-12-27 01:01:55 -0500

    add typing - batch 1

--- streamer/util.py
+++ streamer/util.py
@@ -1,16 +1,17 @@
-def streamify(stream_or_object):
+from typing import Iterable, Iterator, Union, TypeVar
 
-    # iterators w/ py2 support
-    if hasattr(stream_or_object, "__next__") or hasattr(stream_or_object, "next"):
+T = TypeVar('T')
+
+
+def to_iterator(stream_or_object: Union[Iterable[T], Iterator[T], T]) -> Iterator[T]:
+    # iterators
+    if hasattr(stream_or_object, "__iter__") and hasattr(stream_or_object, "__next__"):
         return stream_or_object
 
-    # iterables
-    elif hasattr(stream_or_object, "__iter__") or hasattr(stream_or_object, "__getitem__"):
+    # iterables, but not string
+    elif not isinstance(stream_or_object, str) and \
+            hasattr(stream_or_object, "__iter__") or hasattr(stream_or_object, "__getitem__"):
         return iter(stream_or_object)
 
     else:
-        return iter([stream_or_object])
-
-
-def identity(x):
-    return x
+        return iter((stream_or_object, ))
