bf4ded4dc1caced09caadbe1d07e50de16786158
Arnoldosmium <eaglemewpsy@gmail.com>
2020-12-30 01:19:31 -0500

    docs

--- streamer/streams.py
+++ streamer/streams.py
@@ -15,22 +15,50 @@ T = TypeVar("T")
 
 
 def constant_of(value: T, times: int) -> Stream[T]:
+    """
+    A stream with constant value for x times
+    :param value: constant value
+    :param times: repeat times
+    :return: constant stream
+    """
     return Stream(ConstantOf(value, times))
 
 
 def iterate(seed: T, operator: Callable) -> Stream:
+    """
+    A stream with all result of applying same function on a seed forever.
+    :param seed: the initial value
+    :param operator: the function to apply
+    :return: iterate stream
+    """
     return Stream(RepeatApply(seed, operator))
 
 
 def generate(gen_func: Callable[[], T]) -> Stream[T]:
+    """
+    A stream with all result of rerunning same function forever.
+    :param gen_func: the generator function
+    :return: generate stream
+    """
     return Stream(RepeatApply(None, lambda _: gen_func())).skip(1)
 
 
 def cartesian_product_stream(*streams: Iterable) -> Stream[Tuple]:
+    """
+    A stream containing all possible combinations of elements in given streams.
+    :param streams: streams to perform cartesian product
+    :return: cartesian product stream
+    """
     return Stream(Cartesian(*streams))
 
 
 def cartesian_power(power: int, collection: Collection) -> Stream[Tuple]:
+    """
+    A stream containing all possibility of picking elements x times (with returning) in a given collection
+    :param power: element numbers / self product x times
+    :param collection: the collection
+    :return: cartesion power stream
+    """
     if power <= 0:
         raise ValueError("Cartesian power number should be at least 1")
     return Stream(Cartesian(*(iter(collection) for _ in range(power))))
